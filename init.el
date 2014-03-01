ab00910096e596a3f1e986022aa36fa9e6bf9f84
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-03-01 15:24:06 +0100

    Add headers

--- init.el
+++ init.el
@@ -1,6 +1,13 @@
+;;; clojurescript-pack.el --- Clojurescript configuration
+
+;;; Commentary:
+
+;;; Code:
+
 ;; pre-requisite, install some packs
 (install-packs '(clojurescript-mode
                  cljsbuild-mode))
 
 (add-to-list 'auto-mode-alist '("\.cljs$" . clojurescript-mode))
 
+;;; clojurescript-pack.el ends here
