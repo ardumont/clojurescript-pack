c2aa8d6d97b25c7234de3481450078355d38f668
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-03-03 09:00:31 +0100

    Add smartscan-mode to cljs files

--- init.el
+++ init.el
@@ -6,8 +6,12 @@
 
 ;; pre-requisite, install some packs
 (install-packs '(clojurescript-mode
-                 cljsbuild-mode))
+                 cljsbuild-mode
+                 smartscan))
 
 (add-to-list 'auto-mode-alist '("\.cljs$" . clojurescript-mode))
+
+(require 'smartscan)
+(add-hook 'clojurescript-mode-hook (lambda () (smartscan-mode)))
 
 ;;; clojurescript-pack.el ends here
