69e243b3dcee4b61f1dc6322a33c30896c12900d
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-05-28 22:54:27 +0200

    Deactivate clojurescript

--- clojurescript-pack.el
+++ clojurescript-pack.el
@@ -5,14 +5,14 @@
 ;;; Code:
 
 (require 'install-packages-pack)
-(install-packs '(clojurescript-mode
+(install-packs '(;; clojurescript-mode
                  cljsbuild-mode
                  smartscan))
 
-(add-to-list 'auto-mode-alist '("\.cljs$" . clojurescript-mode))
+;;(add-to-list 'auto-mode-alist '("\.cljs$" . clojurescript-mode))
 
 (require 'smartscan)
-(add-hook 'clojurescript-mode-hook (lambda () (smartscan-mode)))
+;; (add-hook 'clojurescript-mode-hook (lambda () (smartscan-mode)))
 
 (provide 'clojurescript-pack)
 ;;; clojurescript-pack.el ends here
